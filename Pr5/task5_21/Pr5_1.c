dca25bbb23aa1b88fc8547f9b0941ba0b625a506
Daria123H <163399525+Daria123H@users.noreply.github.com>
2025-04-16 16:33:25 +0300

    Update Pr5_1.c

--- Pr5/task5_21/Pr5_1.c
+++ Pr5/task5_21/Pr5_1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+#include <string.h>
+
+int main() {
+  
+    char buffer[5] = "abcd"; 
+  
+    int secret = 12345;        
+
+    printf("buffer[5] = %c\n", buffer[5]); 
+
+    buffer[6] = 'X';
+
+    printf("secret після переповнення буфера = %d\n", secret);
+
+    return 0;
+}
