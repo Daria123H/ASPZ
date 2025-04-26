48c7a3691bf1e676351eb1b03cba127137d3b5eb
Daria123H <163399525+Daria123H@users.noreply.github.com>
2025-04-26 21:43:56 +0300

    Create PR8_22.c

---
+++ Pr8/PR8_2/PR8_22.c
@@ -0,0 +1,18 @@
+// create.c
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    int fd = open("PR8.bin", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1) {
+        perror("Error creating file");
+        return 1;
+    }
+
+    unsigned char data[] = {4, 5, 2, 2, 3, 3, 7, 9, 1, 5};
+    write(fd, data, sizeof(data));
+    close(fd);
+
+    return 0;
+}
