35bc399a30d77ca8e0e8462ea387afc7c0dcb783
Daria123H <163399525+Daria123H@users.noreply.github.com>
2025-04-27 15:06:47 +0300

    Create PR9_4.c

---
+++ Pr9/PR9_4/PR9_4.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    printf("Whoami's team:\n");
+    system("whoami");
+
+    printf("\nRun the id command:\n");
+    system("id");
+
+    return 0;
+}
